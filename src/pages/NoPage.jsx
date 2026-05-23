dad69d58f8e2bab0421155406ad633f0010b4ce0
tanuj <you@3example3.com>
2026-05-23 21:00:53 +0530

    d

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -2,6 +2,36 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function NoPage() {
+
+
+
+
+    const arr = [4, 3, 2, 9];
+
+    var plusOne = function (digits) {
+
+        const last = digits.pop() + 1;
+
+        let data = []
+
+        if (last >= 10) {
+
+            data = last.toString().split('');
+
+        }
+
+
+        const dddd = digits.concat(data);
+
+        console.log(dddd);
+
+    };
+
+
+    plusOne(arr);
+
+
+
     return (
         <>
             <Header />
